912e86226fe11d4680703559f9b3130af79ad78a
Sprite_Spirit <74331153+SpriteSpirit@users.noreply.github.com>
2024-04-11 00:42:18 +0300

    - Create model Product

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Название')
+    description = models.CharField(max_length=1000, verbose_name='Описание')
+    image = models.ImageField(upload_to='goods/', verbose_name='Изображение')
+    category = models.CharField(max_length=100, verbose_name='Категория')
+    price = models.IntegerField(verbose_name='Цена')
+    data_create = models.DateTimeField(verbose_name='Дата создания')
+    data_changed = models.DateTimeField(verbose_name='Дата изменения')
+
+    def __str__(self):
+        return f'{self.name}\n{self.description}\n{self.category}\n{self.price}\n{self.data_create}\n{self.data_changed}'
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
