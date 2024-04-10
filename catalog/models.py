85bfa33e1fc8bc3c63920ac9281789a0c2cbc36f
Sprite_Spirit <74331153+SpriteSpirit@users.noreply.github.com>
2024-04-11 00:53:49 +0300

    - Create ForeignKey (product|category)

--- catalog/models.py
+++ catalog/models.py
@@ -1,9 +1,21 @@
 from django.db import models
 
 
-class Product(models.Model):
+class Category(models.Model):
     name = models.CharField(max_length=100, verbose_name='Название')
-    description = models.CharField(max_length=1000, verbose_name='Описание')
+    description = models.TextField(verbose_name='Описание')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+
+
+class Product(models.Model):
+    name = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='Название')
+    description = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='Описание')
     image = models.ImageField(upload_to='goods/', verbose_name='Изображение')
     category = models.CharField(max_length=100, verbose_name='Категория')
     price = models.IntegerField(verbose_name='Цена')
@@ -11,7 +23,7 @@ class Product(models.Model):
     data_changed = models.DateTimeField(verbose_name='Дата изменения')
 
     def __str__(self):
-        return f'{self.name}\n{self.description}\n{self.category}\n{self.price}\n{self.data_create}\n{self.data_changed}'
+        return f'{self.name}\n{self.category}\n{self.price}'
 
     class Meta:
         verbose_name = 'Товар'
