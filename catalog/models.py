016b4423b3e2f7019ab524dcf636775b829764ab
Sprite_Spirit <74331153+SpriteSpirit@users.noreply.github.com>
2024-04-11 17:29:46 +0300

    - fixed bugs

--- catalog/models.py
+++ catalog/models.py
@@ -20,7 +20,7 @@ class Product(models.Model):
     image = models.ImageField(upload_to='goods/', verbose_name='Изображение')
     category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
-    created_at = models.DateTimeField(default=timezone.now, verbose_name='Дата создания')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
     # manufactured_at = models.DateField(default=timezone.now, verbose_name='Дата производства продукта')
 
