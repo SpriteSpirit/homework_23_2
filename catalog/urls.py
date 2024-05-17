8bfade0c8a32dda1c684687845414d244dfb54e2
Sprite_Spirit <Sprite.spirit.t.me@gmail.com>
2024-05-17 14:56:24 +0300

    Add CBV changes

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
-from .views import home, categories, contacts, get_product_detail
+from .views import HomeView, CategoryListView, ContactsView, ProductDetailView
 from .views import create_product, get_product_list
 
 app_name = 'catalog'
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('categories/', categories, name='categories'),
-    path('contacts/', contacts, name='contacts'),
-    path('<int:pk>/product/', get_product_detail, name='product_detail'),
+    path('', HomeView.as_view(), name='home'),
+    path('categories/', CategoryListView.as_view(), name='categories'),
+    path('contacts/', ContactsView.as_view(), name='contacts'),
+    path('<int:pk>/product/', ProductDetailView.as_view(), name='product_detail'),
     path('create/product/', create_product, name='create_product'),
     path('products/', get_product_list, name='product_list'),
 ]
