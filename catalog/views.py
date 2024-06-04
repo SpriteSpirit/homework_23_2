528f10805e492df57e09fd007284c16279242279
Sprite_Spirit <Sprite.spirit.t.me@gmail.com>
2024-06-05 00:53:31 +0300

    Add in services.py get_categories()

--- catalog/views.py
+++ catalog/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import get_object_or_404
 
 from django.contrib import messages
 
-from .services import get_cached_products
+from .services import get_cached_products, get_categories
 from .utils import slugify
 
 from django.urls import reverse_lazy, reverse
@@ -66,6 +66,7 @@ class CategoryListView(LoginRequiredMixin, ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'КАТАЛОГ'
+        context['categories'] = get_categories()
         return context
 
     def get_queryset(self):
